61814ddee2a65677daa121e6f6adaa2815e6c245
dgellow <samuel.elborai@gmail.com>
2014-05-27 11:30:04 +0200

    Add navigation

---
+++ emacs.d/modules/03-navigation.el
@@ -0,0 +1,47 @@
+;;; navigation.el --- dgellow's navigation configuration.
+;;
+;; Copyright (c) 2014 Samuel El-Borai
+;;
+;; Author: Samuel El-Borai <samuel.elborai@gmail.com>
+;; URL: https://github.com/dgellow/home-bootstrapping/
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not port of GNU Emacs.
+
+;;; Commentary:
+
+;;; License:
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Code:
+(use-package ido
+  :init (ido-mode t)
+  :config (progn
+            (setq ido-enable-flex-matching t)
+            (setq ido-everywhere t)))
+
+(use-package flx-ido
+  :init (flx-ido-mode t)
+  ;; disable ido faces to see flx highlights.
+  :config (setq ido-use-faces nil))
+
+(provide 'navigation)
+;;; navigation.el ends here
