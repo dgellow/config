e6b1d33caf4ef7d1d590c40202143a2e052db82c
dgellow <samuel.elborai@gmail.com>
2014-05-27 07:33:59 +0200

    First working versiong of new init system

---
+++ emacs.d/init.el
@@ -0,0 +1,89 @@
+;;; init.el --- dgellow's configuration entry point.
+;;
+;; Copyright (c) 2014 Samuel El-Borai
+;;
+;; Author: Samuel El-Borai <samuel.elborai@gmail.com>
+;; URL: https://github.com/dgellow/home-bootstrapping/
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not port of GNU Emacs.
+
+;;; Commentary:
+
+;;; License:
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Code:
+(defun dg-match-first (REGEXP STRING)
+  "Return the first group matched by applying REGEXP on STRING."
+  (string-match REGEXP STRING)
+  (match-string 1 STRING))
+
+(defun dg-message (STRING)
+  "Print a message with `load-file-name' as context and STRING as value.
+If `load-file-name' is empty, use the value of `current-buffer'."
+  (let* ((filename (or load-file-name (current-buffer)))
+         (regexp-filename "emacs.d/\\(.*\\.el$\\)")
+         (context (dg-match-first regexp-filename filename)))
+    (if context
+      (message "[%s]: %s" context STRING))))
+
+(dg-message "Welcome in dgellow's Emacs flavour.")
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen
+(setq inhibit-startup-screen t)
+
+;; Package management
+(dg-message "Load the package manager.")
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;; Set paths
+(defvar dg-utils-dir
+  (expand-file-name "utils" user-emacs-directory)
+  "Directory containing utility functions.
+Located under ~/.emacs.d/utils/ .")
+(defvar dg-modules-dir
+  (expand-file-name "modules" user-emacs-directory)
+  "Directory containing modules configuration files.
+Located under  ~/.emacs.d/modules/ .")
+
+(defvar dg-config-dirs
+  (list dg-utils-dir dg-modules-dir)
+  "List of directories containing config files.")
+
+;; Load modules and utils
+(defun dg-load-modules (DIRECTORY)
+  "Load every *.el files in the given DIRECTORY."
+  (when (file-exists-p DIRECTORY)
+    (mapc 'load (directory-files DIRECTORY 't "^[^#].*\\.el$"))))
+
+(dg-message "Load the modularized configuration.")
+(dolist (dir dg-config-dirs)
+        (dg-message (format"- %s" dir))
+        (dg-load-modules dir))
+
+;;; init.el ends here
