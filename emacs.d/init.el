60d21075f3393d7705fbe9e0931adff917830f35
dgellow <samuel.elborai@gmail.com>
2016-12-12 17:44:16 +0100

    improve font and colors

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -108,3 +108,9 @@ Located under  ~/.emacs.d/modules/ .")
     ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/usr/local/bin")))
  '(js2-basic-offset 2)
  '(web-mode-code-indent-offset 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Input Mono" :background "#2B303B" :foreground "#C0C5CE" :slant normal :weight thin :height 130 :width normal)))))
