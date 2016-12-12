180ec91c27614227bcfa3ac4cd099c91c1019a03
dgellow <samuel.elborai@gmail.com>
2016-12-12 17:43:40 +0100

    package-initialize

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -32,6 +32,13 @@
 ;; SOFTWARE.
 
 ;;; Code:
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun dg-match-first (REGEXP STRING)
   "Return the first group matched by applying REGEXP on STRING."
   (string-match REGEXP STRING)
