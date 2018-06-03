f4fe8c5c7a1a218c597d35bd1ed3dd0285bc4c0e
Samuel El-Borai <samuel.elborai@gmail.com>
2018-06-03 13:23:43 +0200

    emacs: comment surgeon.el for now as it causes issues with use-package

--- emacs.d/utils/surgeon.el
+++ emacs.d/utils/surgeon.el
@@ -32,39 +32,40 @@
 ;; SOFTWARE.
 
 ;;; Code:
-(use-package thingatpt)
-
-(defun select-current-word ()
-  "Select as a region the word at point."
-  (let ((bounds (bounds-of-thing-at-point 'symbol)))
-    (push-mark bounds)
-    ()))
-
-(defun select-current-sexp ()
-  "Select as a region the s-expression (or what your current language mode considers as one) at point."
-  (interactive)
-  (let (sum-run start-position end-position)
-    (skip-chars-backward "^\(")
-    (setq start-position (- (point) 1))
-    ;; (skip-chars-forward "^\)")
-    ;; (setq end-position (point))
-
-    ;; Loop on each chars
-    ;; -1 if ')'
-    ;; +1 if '('
-    ;; stop loop if 0
-    (while (not (= sum-run 1))
-      ;; TODO
-      )
-    (goto-char start-position)))
-
-(defun delete-current-word ()
-  "Delete the word at point."
-  ())
-
-(defun delete-current-sexp ()
-  "Delete the sexp at point."
-  ())
-
-(provide 'surgeon)
+;;
+;; (use-package thingatpt)
+;;
+;; (defun select-current-word ()
+;;   "Select as a region the word at point."
+;;   (let ((bounds (bounds-of-thing-at-point 'symbol)))
+;;     (push-mark bounds)
+;;     ()))
+;;
+;; (defun select-current-sexp ()
+;;  "Select as a region the s-expression (or what your current language mode considers as one) at point."
+;;  (interactive)
+;;  (let (sum-run start-position end-position)
+;;    (skip-chars-backward "^\(")
+;;    (setq start-position (- (point) 1))
+;;    ;; (skip-chars-forward "^\)")
+;;    ;; (setq end-position (point))
+;;
+;;    ;; Loop on each chars
+;;    ;; -1 if ')'
+;;    ;; +1 if '('
+;;    ;; stop loop if 0
+;;    (while (not (= sum-run 1))
+;;      ;; TODO
+;;      )
+;;    (goto-char start-position)))
+;;
+;;(defun delete-current-word ()
+;;  "Delete the word at point."
+;;  ())
+;;
+;;(defun delete-current-sexp ()
+;;  "Delete the sexp at point."
+;;  ())
+;;
+;;(provide 'surgeon)
 ;;; surgeon.el ends here
